1522183e7f7c8f02e7979093d925e718bcf9dbc0
Jaeden Thia <jaeden@Jaedens-MacBook-Pro.local>
2025-02-16 04:36:27 +0800

    Milestone done

--- src/routes/milestones/+page.svelte
+++ src/routes/milestones/+page.svelte
@@ -1,7 +1,15 @@
 <script>
-    let percentage = 20; // Initial percentage
+    let days_till_next_mile = 25; 
+    let widthbar = days_till_next_mile/30 * 70;
+    
+    let percentage = Math.round(days_till_next_mile/30 * 1000)/10;
+    
     let streak = 20;
-    let level = 1;
+    let level = 6;
+    let money_donated_so_far = 1500;
+    let money_contributed_by_donors = 1200;
+    let money_donated_personally = 300;
+    let total_days_without_addiction = 75;
 
     function increaseStreak() {
         streak += 1; // Increment the streak by 1
@@ -17,14 +25,23 @@
 
     <div class="rectangle">
         <div class="background-bar"></div>
-        <div class="milestone-bar" style="width: {percentage}%"></div>
+        <div class="milestone-bar" style="width: {widthbar}%"></div>
         <img src="/images/MessageBox/MilestoneBar.svg" alt="BarFrame" class="barframe">
-      </div>
-    
-    <div class="streakcontain">
-        <img src="/images/MessageBox/StreaksPlannet.svg" alt="Streaks" class="streaksplannet">
-        <label for="streak">Streak: </label>
-        <input type="text" id="streak" bind:value={streak} readonly />
+        <div class="text-bar">
+            <h1>{percentage}%</h1>
+        </div>
+        <div class="text-box">
+            <h2>My Milestones</h2>
+        </div>
+
+        <div class="text-box-more">
+            <p><strong>Money Donated So Far:</strong> ${money_donated_so_far}</p>
+            <p><strong>Money Contributed by Donors:</strong> ${money_contributed_by_donors}</p>
+            <p><strong>Money Donated Personally:</strong> ${money_donated_personally}</p>
+            <p><strong>Level of Alien:</strong> {level}</p>
+            <p><strong>Total Days Without Addiction:</strong> {total_days_without_addiction} days</p>
+            <p><strong>Days to next milestone:</strong> {days_till_next_mile} days</p>
+        </div>
     </div>
 
     <div class="container">
@@ -32,24 +49,67 @@
         <img src="/images/alien/Alienbg.png" alt="Alien Background" class="alien-bg" />
         <img src="/images/alien/Baby{level}.png" alt="Alien" class="alien">
       </div>
+
+    <div class="streakcontain">
+        <img src="/images/MessageBox/StreaksPlannet.svg" alt="Streaks" class="streaksplannet">
+        <label for="streak">Streak: </label>
+        <input type="text" id="streak" bind:value={streak} readonly />
+    </div>
+
 </div>
 
 <style>
+.text-bar {
+    position: absolute;
+    top: 78%;
+    left: 42%;
+    width: 60%;
+    color: #5A3878;
+}
+
+.text-box-more {
+    position: absolute;
+    top: 12%;
+    left: 9%;
+    width: 60%;
+}
+
+p {
+    font-size: 1.5vw;
+    margin: 1.2vw 0;
+    line-height: 1.6;
+    color: #E2C8DC;
+}
+
+.text-box-more p::before {
+    content: url('/images/BulletPointStar.svg');  /* Use your SVG image */
+    margin-right: 10px;
+}
+
+.text-box {
+    position: absolute;
+    top: 4%;
+    left: 38%;
+}
+
+h2 {
+    font-size: 40px;
+    font-weight: bold;
+    color: #E2C8DC;
+    text-decoration: underline;
+    text-underline-offset: 5px; /* Adjust underline spacing */
+}
 
 .alien {
     position: absolute;
-    top: 53%;
-    left: 50%;
+    top: 51%;
+    left: 48%;
     transform: translate(-50%, -50%);
-    width: 88%;
+    width: 79%;
     height: auto;
     max-width: 100%;
     border-radius: 20px;
     animation: bounce 0.8s ease-in-out;
-}
-
-.alien:hover {
-    animation: bounce 0.6s ease-in-out; /* Restart bounce */
 }
 
 @keyframes bounce {
@@ -61,8 +121,8 @@
     position: absolute;
     width: 62%;
     height: 68%;
-    top: 50%;
-    left: 39%;
+    top: 51%;
+    left: 84%;
     transform: translate(-50%, -50%);
 }
 
@@ -71,7 +131,7 @@
     bottom: 13%;
     left: 11%;
     height: 10%;
-    background-color: #4caf50;
+    background: linear-gradient(to right, #1EB713, #42E637, #1EB713);;
     transition: width 0.5sease;
   }
 
@@ -142,7 +202,7 @@ input {
 .container {
     position: relative;
     display: inline-block;
-    top: 24%;
+    top: 25%;
     left: 64%;
     width: 30%;
 }
@@ -169,7 +229,7 @@ input {
 .rectangle {
     position: absolute;
     width: 62%;
-    height: 68%;
+    height: 66%;
     background: #5A3878;
     top: 50%;
     left: 39%;
