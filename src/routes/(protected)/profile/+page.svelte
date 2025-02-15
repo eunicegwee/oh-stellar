58f05c1ae57902bb6c21c5df920dd7957793513b
Eunice <eunicegweee@gmail.com>
2025-02-16 06:17:17 +0800

    Slight changes to all pages

--- src/routes/(protected)/profile/+page.svelte
+++ src/routes/(protected)/profile/+page.svelte
@@ -18,14 +18,16 @@
     }
 
     onMount(() => {
+        const starContainer = document.querySelector('.star-container');
+
         if (star) {
             star.style.position = "relative";
-            star.style.width="80%";
-            // star.style.transform = "translate(-50%, -50%)";
+            star.style.height="100%";
+            star.style.width="60%";
+            star.style.transform = "translate(-50%, -50%)";
             star.style.visibility = "visible";
-            star.style.paddingLeft="40px";
-            star.style.top="40%";
-            star.style.left="40%";
+            star.style.top="50%";
+            star.style.left="50%";
         }
 
     })
@@ -34,17 +36,21 @@
   
 <div class="galaxy">
     <img src="/images/galaxy/galaxy-bg.jpg" alt="galaxy" id="galaxy-bg" >
-    <div class="star">
-        <Star level={userLevel} bind:star={star}/>
-        <p class="username">{username}'s star</p>   
-        <img class="smallstar" src="/images/small-star-3.svg" alt="small star">
-        <img class="smallstar2" src="/images/small-star-2.svg" alt="small star 2">  
+    <div class="star-container">
+        <div class="star">
+            <Star level={userLevel} bind:star={star}/>  
+            <img class="smallstar" src="/images/small-star-3.svg" alt="small star">
+            <img class="smallstar2" src="/images/small-star-2.svg" alt="small star 2">  
+        </div>
+        <p class="username">{username}'s Star</p> 
     </div>
     
     <div class="details">
       <div class="profile">
-        <img src="/images/BulletPointStar.svg" alt="profile star" style="padding: 20px; margin-bottom: 10px">
-        <p>Profile</p>
+        <div class="profile-title">
+            <img src="/images/BulletPointStar.svg" alt="profile star">
+            <p>Profile</p>
+        </div>
         <button class="edit-button" on:click={changeEditStatus}>
             {#if editStatus}
                 <img src="/images/Vector.svg" alt="tick">              
@@ -70,6 +76,7 @@
         align-items: center;
         justify-content: center;
         padding: 140px;
+        gap: 20px;
         position: relative;
       }
   
@@ -78,62 +85,81 @@
       height: 30%;
       position: relative;
       width: 15%;
-      bottom: 110%;
-      left: 10%;
-      
+      left: 20%;
+      bottom: 100%;
     }
   
     .smallstar2{
       height: auto;
       width: 15%;
       position: relative;
-      left: 40%;
+      left: 50%;
+      bottom: 30%;
     }
   
     .details {
       background-color: #583878;
       border-radius: 20px;
-      height: 600px;
       width: 50%;
       box-sizing: border-box; 
       display: flex;
       flex-direction: column;
-      padding: 30px;
+      padding: 50px;
       z-index: 1;
     }
  
     .profile {
-      color: #E2C8DC;
-      text-align: center;
-      font-family: 'Lato';
-      font-size: 3vw;
-      font-weight: bolder;
-      display: flex;
-      flex-direction: row;
-      justify-content: center;
-      
+        color: #E2C8DC;
+        text-align: center;
+        font-family: 'Lato';
+        font-size: 3vw;
+        font-weight: bolder;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+    }
+
+    .profile-title {
+        display: flex;
+        flex-direction: row;
+        gap: 10px;
+        align-items: center;
+        justify-content: center;
+        height: 3vw;
+    }
+
+    .profile-title img {
+        height: 3vw;
+    }
+
+    .profile-title p {
+        margin: 0;
+    }
+
+    .star-container {
+        height: 100%;
+        width: 50%;
+        z-index: 1;
+        position: relative;
     }
   
     .star {
-      height: 100%;
-      width: 50%;
-      z-index: 1;
-      position: relative;
+        height: 80%;
     }
-
 
     .username {
         font-family: "Lato";
         font-size: 50px;
-        color: white;
+        font-weight: bold;
+        color: #E2C8DC;
         position: relative;
         display: flex;
         z-index: 1;
         flex-wrap: wrap;
         align-content: flex-end;
         justify-content: space-evenly;
-        top: 30%;
-        right: 5%;
+        /* top: 30%;
+        right: 5%; */
     }
 
     .edit-button {
@@ -149,6 +175,7 @@
         width: 100%;
         display: flex;
     }
+    
     #galaxy-bg {
         position: absolute;
         width: 100%;
